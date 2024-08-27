4e41240ee83cb9cdb4e9c5fc3385692f62503525
Bilal Ali <bilalali@Bilals-MacBook-Pro-6223.local>
2024-08-27 16:41:12 -0700

    Duration

--- Frontend/src/Components/Form.js
+++ Frontend/src/Components/Form.js
@@ -7,13 +7,15 @@ import React, { useState } from 'react';
 const Form = ({ onClose }) => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
+  const [duration, setDuration] = useState('30 mins');
   const [loading, setLoading] = useState(false);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    const formData = { name, phone };
+    const formData = { name, phone, duration };
 
     try {
       const response = await fetch('/api/users', {
@@ -48,10 +50,20 @@ const Form = ({ onClose }) => {
       <span className="close-button" onClick={onClose}>&times;</span>
       <form>
         <h2>Add Your Name to the List!</h2>
-        <label htmlFor="name">Name:</label>
+        <label htmlFor="name">Full Name:</label>
         <input type="text" id="name" name="name" required onChange={(e) => setName(e.target.value)}/>
         <label htmlFor="phone">Phone Number:</label>
         <input type="tel" id="phone" name="phone" required onChange={(e) => setPhone(e.target.value)}/>
+        <label htmlFor="duration">Select Duration: </label>
+        <select 
+        id="duration" 
+        name="duration" 
+        required 
+        onChange={(e) => setDuration(e.target.value)}>
+        <option value="30 mins">30 mins</option>
+        <option value="45 mins">45 mins</option>
+        <option value="1 hour">1 hour</option>
+        </select>
         <button type="submit" disabled={loading}>{loading ? 'Submitting...' : 'Submit'}</button>
       </form>
     </div>
